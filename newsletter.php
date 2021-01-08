37114864dd6cd0596eedd815d6f6de1c36b0190d
El babas <babas@iMac-de-Imac.local>
2021-01-08 01:20:32 -0600

    new branch

--- newsletter.php
+++
@@ -1,9 +0,0 @@
-<section class="newsletter">
-        <div>
-            <p>Entérate de las noticias antes que nadie en nuestro Newsletter</p>
-            <form action="">
-                <input type="text" name="usuario" id="usuario" placeholder="Escribe tu correo electrónico">
-                <button class="">SUSCRÍBETE</button>
-            </form>
-        </div>
-    </section>
