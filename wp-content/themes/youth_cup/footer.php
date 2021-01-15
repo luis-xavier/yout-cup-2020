4093d1a86ffa5b95243b9a4690b1f8efbc902fdf
El babas <babas@iMac-de-Imac.local>
2021-01-14 20:00:24 -0600

    set class on menu footer

--- wp-content/themes/youth_cup/footer.php
+++ wp-content/themes/youth_cup/footer.php
@@ -52,9 +52,9 @@
             <nav role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
-    					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+    					'container_class' => 'third-subsection-footer subsection-footer',         // class of container (should you choose to use it)
     					'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
-    					'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+    					'menu_class' => '',            // adding custom nav class
     					'theme_location' => 'footer-links',             // where it's located in the theme
     					'before' => '',                                 // before the menu
     					'after' => '',                                  // after the menu
