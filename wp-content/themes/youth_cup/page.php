8130e77736f014e228ff63cb1d499c46337b4113
El babas <babas@iMac-de-Imac.local>
2021-01-19 05:34:06 -0600

    set page template

--- wp-content/themes/youth_cup/page.php
+++ wp-content/themes/youth_cup/page.php
@@ -1,67 +1,42 @@
 <?php get_header(); ?>
 
-			<div id="content">
 
-				<div id="inner-content" class="wrap cf">
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="noticia-container">
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-								<header class="article-header">
 
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+<?php 
 
-									<p class="byline vcard">
-										<?php printf( __( 'Posted', 'bonestheme').' <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> '.__( 'by',  'bonestheme').' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
+$thumb_id = get_post_thumbnail_id();
+$thumb_url = wp_get_attachment_image_src($thumb_id,'large', true);
+$url_imagen=$thumb_url[0];
 
-								</header> <?php // end article header ?>
+?>
+    <!--img src="<?= $url_imagen ?>" class="first-image" alt=""-->
+    <h2><?php the_title(); ?></h2>
+    <p><?php  the_content(); ?></p>
+	
+</div>
 
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
+<?php endwhile; ?>
 
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section> <?php // end article section ?>
+<?php else : ?>
 
-								<footer class="article-footer cf">
+	<article id="post-not-found" class="hentry cf">
+			<header class="article-header">
+				<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+			</header>
+			<section class="entry-content">
+				<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+			</section>
+			<footer class="article-footer">
+					<p><?php _e( 'This is the error message in the single.php template.', 'bonestheme' ); ?></p>
+			</footer>
+	</article>
 
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; endif; ?>
-
-						</main>
-
-						<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+<?php endif; ?>
 
 <?php get_footer(); ?>
