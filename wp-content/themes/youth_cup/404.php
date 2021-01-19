2384c5a95d6c1f1a32e8d619578fd232d335b80e
El babas <babas@iMac-de-Imac.local>
2021-01-19 06:46:58 -0600

    fix conteiner height on 404 template

--- wp-content/themes/youth_cup/404.php
+++ wp-content/themes/youth_cup/404.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 
-<div class="noticia-container">
+<div class="noticia-container" style="min-height: 400px;">
 
 <h1><?php _e( 'Epic 404 - Page not found', 'bonestheme' ); ?></h1>
 
