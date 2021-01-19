72af1341f58fc502dcf52cc55511c07b44968261
El babas <babas@iMac-de-Imac.local>
2021-01-19 06:06:57 -0600

    section news tamplate

--- wp-content/themes/youth_cup/archive.php
+++ wp-content/themes/youth_cup/archive.php
@@ -1,74 +1,60 @@
 <?php get_header(); ?>
 
-			<div id="content">
+<?php 
+    $latest = new WP_Query(
+        array(
+            'post_type' => 'car',
+            'post_status' => 'publish',
+            'posts_per_page' => 1,
+            'orderby' => 'modified',
+            'order' => 'DESC'
+        )
+    );
 
-				<div id="inner-content" class="wrap cf">
+    if($latest->have_posts()){
+        $modified_date = $latest->posts[0]->post_modified;
+    }
+?>
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+<section class="np-wrapper">
+    <div class="noticia-principal noticia">
+	<?php (has_post_thumbnail() ? the_post_thumbnail() : false) ?>
+        <article>
+            <h2><?php the_title(); ?></h2>
+			<p><?php the_excerpt(); ?></p>
+			<a href="<?= esc_url( get_permalink( )) ?>" class="boton boton-reverse">leer más</a>
+        </article>
+    </div>
 
-							<?php
-							the_archive_title( '<h1 class="page-title">', '</h1>' );
-							the_archive_description( '<div class="taxonomy-description">', '</div>' );
-							?>
-							
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<section class="noticias">
+<div id="display-noticia" class="noticias-wrapper nw-noticia">
+	<?php 
+	$contador = 1;
+	if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+    
+        
+            <div id="noticia-<?= $contador?>" class="noticia">
+			<?php (has_post_thumbnail() ? the_post_thumbnail('thumbnail', array('class' => 'img-np')) : false) ?>
+                <h3><?php the_title(); ?></h3>
+                <p><?php the_excerpt(); ?></p>
+                <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">LEER MÁS</a>
+            </div>
 
-								<header class="entry-header article-header">
+			<?php
+					$contador ++; 
+				endwhile; ?>
 
-									<h3 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-									<p class="byline entry-meta vcard">
-										<?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                  							     /* the time the post was published */
-                  							     '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__('by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
 
-								</header>
 
-								<section class="entry-content cf">
+<?php else : ?>
 
-									<?php the_post_thumbnail( 'bones-thumb-300' ); ?>
+<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
 
-									<?php the_excerpt(); ?>
-
-								</section>
-
-								<footer class="article-footer">
-
-								</footer>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php bones_page_navi(); ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-										<header class="article-header">
-											<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-										<section class="entry-content">
-											<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the archive.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
-
-						</main>
-
-					<?php get_sidebar(); ?>
-
-				</div>
-
-			</div>
+<?php endif; ?>
+</div>
+</div>
+    </section>
+</section>
 
 <?php get_footer(); ?>
