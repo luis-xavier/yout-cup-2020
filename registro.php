2e01bdbefc9d99e5203b6052a125209e6b5fee8a
luis-xavier <luis_xavier_x@yahoo.com.mx>
2021-01-20 01:53:24 -0600

    Formulario de jugadores

--- registro.php
+++ registro.php
@@ -1,103 +1,91 @@
 <?php include "header.php" ?>
 
 <link rel="stylesheet" type="text/css" href="css/formulario.css">
-<script type="text/javascript" src="js/formulario.js"></script>
+<link rel="stylesheet" type="text/css" href="css/registro.css">
 
 <div id="back_form_login">
 <section id="form-wrapper">
 	<h1>Registra a tus jugadores</h1>
     <p>Selecciona en el apartado de cada jugador para registrarlo con su nombre completo, puedes borrar y editar sus datos también.</p>
-    <form action="" method="POST" id="formulario" class="formulario formulario-registro">  
-        <label for="Jugador" class="jugador">Jugador 1</label>          
-        <ul class="container-first-i">
-            <li class="form-jugador">
-                <input id="nombre_completo" maxlength="50" name="nombre" size="20" type="text" required="" class="sinvalidar" onBlur="validarNombreEquipo('nombre_equipo')" oninput="validarNombreEquipo('nombre_equipo')"/>
-                <label for="nombre_completo" class="nombre-completo">Nombre completo:</label>
-            </li>
-            <li class="fecha" style="margin-bottom: 15px;">
-                <label for="Fecha">Fecha de nacimiento:</label>
-                <div class="date-selector"> 
-                    <select name="año" id="año">
-                        <option value="AAAA">Año</option>
-                        <option value="2004">2004</option>
-                        <option value="2005">2005</option>
-                        <option value="2006">2006</option>
-                        <option value="2007">2007</option>
-                    </select>
-                    <select name="meses" id="meses">
-                        <option value="MM">Mes</option>
-                        <option value="Enero">01</option>
-                        <option value="Febrero">02</option>
-                        <option value="Marzo">03</option>
-                        <option value="Abril">04</option>
-                        <option value="Mayo">05</option>
-                        <option value="Junio">06</option>
-                        <option value="Julio">07</option>
-                        <option value="Agosto">08</option>
-                        <option value="Septiembre">09</option>
-                        <option value="Octubre">10</option>
-                        <option value="Noviembre">11</option>
-                        <option value="Diciembre">12</option>
-                    </select>
-                    <select name="dias" id="dias">
-                        <option value="DD">Día</option>
-                        <option value="1">01</option>
-                        <option value="2">02</option>
-                        <option value="3">03</option>
-                        <option value="4">04</option>
-                        <option value="5">05</option>
-                        <option value="6">06</option>
-                        <option value="7">07</option>
-                        <option value="8">08</option>
-                        <option value="9">09</option>
-                        <option value="10">10</option>
-                        <option value="11">11</option>
-                        <option value="12">12</option>
-                        <option value="13">13</option>
-                        <option value="14">14</option>
-                        <option value="15">15</option>
-                        <option value="16">16</option>
-                        <option value="17">17</option>
-                        <option value="18">18</option>
-                        <option value="19">19</option>
-                        <option value="20">20</option>
-                        <option value="21">21</option>
-                        <option value="22">22</option>
-                        <option value="23">23</option>
-                        <option value="24">24</option>
-                        <option value="25">25</option>
-                        <option value="26">26</option>
-                        <option value="27">27</option>
-                        <option value="28">28</option>
-                        <option value="29">29</option>
-                        <option value="30">30</option>
-                        <option value="31">31</option>
-                    </select> 
-                </div>                              
-            </li>
-            <li style="text-align: left; margin-bottom: 10px;">
-                <input type="checkbox" class="checkbox-estilizado" name="aviso" id="aviso" onchange="">
-                <label for="aviso">Cuenta con pasaporte vigente</label></li>
-            <li>
-                <button type="submit" name="enviar" onclick="validacion()" class="boton boton-registro" style="margin-bottom: 10px;">ACEPTAR</button>
-            </li>
-        </ul>
-    </form>
-    <form action="" method="POST" id="formulario" class="formulario formulario-registro cerrado">
-    <label for="Jugador" class="jugador">Jugador 2</label> 
-        <ul class="container-first-i">
-            <li class="form-jugador form-jugador-cerrado">
-                <div class="div-form">
-                    <button><img src="img/edit.svg" alt=""></button>
-                    <button><img src="img/cross.svg" alt=""></button>
-                </div>
-            </li>
-        </ul>  
-    </form>
-    <button type="submit" name="enviar" onclick="validacion()" class="boton">FINALIZAR REGISTRO</button> 
+	
+	<form action="" method="POST" id="formulario" class="formulario formulario-registro">
+	    <?php
+		    $numjugador = 1;
+		    while($numjugador <= 15):
+	    ?>
+		<div id="container-jugador-<?= $numjugador ?>" class="container-jugador">
+			<h5>Jugador <?= $numjugador ?></h5>
+			<div class="cuadro-datos cerrado">
+				<div class="jugadores-botones">
+					<p>Nombre del jugador</p>
+					<!--a href="#" class="jugadores-cerrar"><img src="img/jugador-close.svg"></a-->
+					<a href="#" class="jugadores-editar"><img src="img/jugador-edit.svg"></a>
+				</div>
+				<div class="jugadores-formulario">
+					<ul>
+						<li>
+							<input id="nombre-<?= $numjugador ?>" maxlength="50" name="nombre-<?= $numjugador ?>" size="20" type="text" required="" class="sinvalidar nombre-jugador" onBlur="//validarNombre('nombre-<?= $numjugador ?>')" oninput="//validarNombre('nombre-<?= $numjugador ?>')"/>
+	            			<label for="nombre-<?= $numjugador ?>">Nombre completo:</label>
+						</li>
+						<li>
+							<label for="año-<?= $numjugador ?>">Fecha de nacimiento</label>
+							<div class="date-selector"> 
+			                    <select name="año-<?= $numjugador ?>" id="año-<?= $numjugador ?>" class="año">
+			                        <option value="AAAA">Año</option>
+			                        <option value="2004">2004</option>
+			                        <option value="2005">2005</option>
+			                        <option value="2006">2006</option>
+			                        <option value="2007">2007</option>
+			                    </select>
+			                    <select name="meses-<?= $numjugador ?>" id="meses-<?= $numjugador ?>" class="meses">
+			                        <option value="MM">Mes</option>
+			                        <option value="enero">01</option>
+			                        <option value="febrero">02</option>
+			                        <option value="marzo">03</option>
+			                        <option value="abril">04</option>
+			                        <option value="mayo">05</option>
+			                        <option value="junio">06</option>
+			                        <option value="julio">07</option>
+			                        <option value="agosto">08</option>
+			                        <option value="septiembre">09</option>
+			                        <option value="octubre">10</option>
+			                        <option value="noviembre">11</option>
+			                        <option value="diciembre">12</option>
+			                    </select>
+			                    <select name="dias-<?= $numjugador ?>" id="dias-<?= $numjugador ?>" class="dias">
+			                        <option value="DD">Día</option>
+			                    </select> 
+	                		</div>
+						</li>
+						<li>
+							<label for="talla-<?= $numjugador ?>">Talla de la camiseta</label>
+							<select name="talla-<?= $numjugador ?>" id="talla-<?= $numjugador ?>" class="talla">
+								<option value="talla"></option>
+			                    <option value="xg">XG</option>
+			                    <option value="l">G</option>
+			                    <option value="m">M</option>
+			                    <option value="ch">CH</option>
+			                </select>
+						</li>
+
+						<li style="text-align: left; margin-bottom: 10px;">
+			                <input type="checkbox" class="checkbox-estilizado" name="pasaporte-<?= $numjugador ?>" id="pasaporte-<?= $numjugador ?>" onchange="">
+			                <label for="pasaporte-<?= $numjugador ?>">Cuenta con pasaporte vigente</label></li>
+			            	<a href="#" class="boton boton-registro">ACEPTAR</a>
+					</ul>
+				</div>
+			</div>
+	
+	    </div>
+	    <?php 
+	    $numjugador++;
+		endwhile;
+		?>
+		<button type="submit" name="enviar" onclick="//validacion()" class="boton">FINALIZAR REGISTRO</button> 
+	</form>
 </section>
 </div>
 
-<?php include "modal.php" ?>
-
 <?php include "footer.php" ?>
+
+<script type="text/javascript" src="js/registro.js"></script>
