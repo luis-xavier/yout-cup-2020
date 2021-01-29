11c429c1352287eb5b68d8d70518ffc8ea0339fe
El babas <babas@iMac-de-Imac.local>
2021-01-29 03:14:54 -0600

    mails de confirmacion ok

--- wp-content/themes/youth_cup/library/mailer.php
+++ wp-content/themes/youth_cup/library/mailer.php
@@ -2,46 +2,109 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require_once('/Users/babas/proyectos/xochimaco/fucho/wp-includes/PHPMailer/Exception.php');
-require_once('/Users/babas/proyectos/xochimaco/fucho/wp-includes/PHPMailer/PHPMailer.php');
-require_once('/Users/babas/proyectos/xochimaco/fucho/wp-includes/PHPMailer/SMTP.php');
+require_once(ABSPATH . WPINC . '/PHPMailer/Exception.php');
+require_once(ABSPATH . WPINC . '/PHPMailer/PHPMailer.php');
+require_once(ABSPATH . WPINC . '/PHPMailer/SMTP.php');
 
-//require_once(ABSPATH . WPINC . '/PHPMailer/Exception.php');
-//require_once(ABSPATH . WPINC . '/PHPMailer/PHPMailer.php');
-//require_once(ABSPATH . WPINC . '/PHPMailer/SMTP.php');
-
-//$phpmailer = new PHPMailer\PHPMailer\PHPMailer();
+//init phpmailer
 
 $mail = new PHPMailer(true);
 
+//Server settings
+//$mail->SMTPDebug = 2;
+$mail->Debugoutput = 'html';
+$mail->isSMTP();
+$mail->Host       = 'smtp.gmail.com';
+$mail->SMTPAutoTLS = false; 
+$mail->Port       = 587;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPAuth = true;
+$mail->Username = 'contacto@curveball.mx';
+$mail->Password = 'Contacto2021?';
 
-try {
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.google.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'contacto@curveball.mx';                     // SMTP username
-    $mail->Password   = 'Contacto2021?';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+
+function palEquipo ($paraMail, $paraName) {
+    global $mail;
     //Recipients
     $mail->setFrom('contacto@curveball.mx', 'Contacto Curveball ');
-    $mail->addAddress('miguel.adrian.trejo@gmail.com', 'El babas');     // Add a recipient
+    $mail->addAddress($paraMail, $paraName);
     $mail->addReplyTo('contacto@curveball.mx', 'Contacto Curveball');
 
 
     // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->isHTML(true);
+    $mail->Subject = 'Registro Bayern Youth Cup';
+          $mail->Body    = "<html><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='IE=edge'>
+              <title>Registro Bayern Youth Cup</title>
+              <style type='text/css'>
+                  html, body{margin: 0 !important;padding: 0 !important;height: 100% !important;width: 100% !important;}*{-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;}.ExternalClass{width: 100%;}div[style*='margin: 16px 0']{margin: 0 !important;}table, td{mso-table-lspace: 0pt !important;mso-table-rspace: 0pt !important;}table{border-spacing: 0 !important;border-collapse: collapse !important;table-layout: fixed !important;margin: 0 auto !important;}table table table{table-layout: auto;}img{-ms-interpolation-mode: bicubic;}.yshortcuts a{border-bottom: none !important;}a[x-apple-data-detectors]{color: inherit !important;}
+              </style>
+              <style type='text/css'>
+                  .button-td, .button-a{transition: all 100ms ease-in;}.button-td:hover, .button-a:hover{background: #555555 !important; border-color: #555555 !important;}@media screen and (max-width: 600px){.email-container{width: 100% !important;}.fluid, .fluid-centered{max-width: 100% !important; height: auto !important; margin-left: auto !important; margin-right: auto !important;}.fluid-centered{margin-left: auto !important; margin-right: auto !important;}.stack-column, .stack-column-center{display: block !important; width: 100% !important; max-width: 100% !important; direction: ltr !important;}.stack-column-center{text-align: center !important;}.center-on-narrow{text-align: center !important; display: block !important; margin-left: auto !important; margin-right: auto !important; float: none !important;}table.center-on-narrow{display: inline-block !important;}}
+              </style>
+          </head>
+          <body bgcolor='#FFFFFF' width='100%' style='margin: 0;' yahoo='yahoo'>
+              <table bgcolor='#FFFFFF' cellpadding='0' cellspacing='0' border='0' height='100%' width='100%' style='border-collapse:collapse;'>
+                  <tr>
+                      <td>
+                          <center style='width: 100%;'>
+                              <table align='center' width='600' class='email-container' style='background:#DCE2E5;'>
+                                  <tr>
+                                      <td style='padding: 20px 0; text-align: center'><img src='http://xochimaco.com/youth-cup/img/logo-mail.png' width='334' height='48' alt='alt_text' border='0'></td>
+                                  </tr>
+                              </table>
+                              <table cellspacing='0' cellpadding='0' border='0' align='center' bgcolor='#F3F5F6' width='600' class='email-container'>
+                                  <tr>
+                                      <td style='padding: 40px; text-align: left; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555;'>
+                                          <h1 style='font-size: 20px;'>Gracias por tu registro ".$paraName."</h1>
+                                          <br>
+                                          <p>Hemos recibido tus datos, pronto estaremos en contacto contigo para dar seguimiento a tu proceso de inscripción</p>
+                                  </tr>
+                                  <table align='center' width='600' class='email-container' style='background: #001829;'>
+                                      <tr>
+                                          <td style='padding: 20px 10px 9px;width: 100%;font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height:18px; text-align: center; color: #F3F5F6;'><span class='mobile-link--footer'>Bayern Youth Cup 2021</span> <br><br></tr>
+                                  </table></center></td></tr></table></body></html>";   
 
     $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+function palAdmin ($nombre_equipo,$direccion,$pais,$ciudad,$cp,$telefono,$nombre,$email,$yourrelation,$enteraste,$reasontoregister,$afiliacion) {
+    global $mail;
+    //Recipients
+    $mail->setFrom('contacto@curveball.mx', 'Contacto Curveball ');
+    $mail->addAddress('contacto@curveball.mx', 'Registro en linea');
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Registro Bayern Youth Cup';
+          $mail->Body    = "<!doctype html><html><head><meta charset='UTF-8'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta http-equiv='X-UA-Compatible' content='IE=edge'>
+              <title>Registro Bayern Youth Cup</title>
+              <style type='text/css'>
+                  html, body{margin: 0 !important;padding: 0 !important;height: 100% !important;width: 100% !important;}*{-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;}.ExternalClass{width: 100%;}div[style*='margin: 16px 0']{margin: 0 !important;}table, td{mso-table-lspace: 0pt !important;mso-table-rspace: 0pt !important;}table{border-spacing: 0 !important;border-collapse: collapse !important;table-layout: fixed !important;margin: 0 auto !important;}table table table{table-layout: auto;}img{-ms-interpolation-mode: bicubic;}.yshortcuts a{border-bottom: none !important;}a[x-apple-data-detectors]{color: inherit !important;}
+              </style>
+              <style type='text/css'>
+                  .button-td, .button-a{transition: all 100ms ease-in;}.button-td:hover, .button-a:hover{background: #555555 !important; border-color: #555555 !important;}@media screen and (max-width: 600px){.email-container{width: 100% !important;}.fluid, .fluid-centered{max-width: 100% !important; height: auto !important; margin-left: auto !important; margin-right: auto !important;}.fluid-centered{margin-left: auto !important; margin-right: auto !important;}.stack-column, .stack-column-center{display: block !important; width: 100% !important; max-width: 100% !important; direction: ltr !important;}.stack-column-center{text-align: center !important;}.center-on-narrow{text-align: center !important; display: block !important; margin-left: auto !important; margin-right: auto !important; float: none !important;}table.center-on-narrow{display: inline-block !important;}}
+              </style>
+          </head>
+          <body bgcolor='#FFFFFF' width='100%' style='margin: 0;' yahoo='yahoo'>
+              <table bgcolor='#FFFFFF' cellpadding='0' cellspacing='0' border='0' height='100%' width='100%' style='border-collapse:collapse;'>
+                  <tr>
+                      <td>
+                          <center style='width: 100%;'>
+                              <div style='display:none;font-size:1px;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;mso-hide:all;font-family: sans-serif;'>Tienes una solicitud de inscripción nueva</div>
+                              <table align='center' width='600' class='email-container' style='background:#DCE2E5;'>
+                                  <tr>
+                                      <td style='padding: 20px 0; text-align: center'><img src='http://xochimaco.com/youth-cup/img/logo-mail.png' width='334' height='48' alt='alt_text' border='0'></td>
+                                  </tr>
+                              </table>
+                              <table cellspacing='0' cellpadding='0' border='0' align='center' bgcolor='#F3F5F6' width='600' class='email-container'>
+                                  <tr>
+                                      <td style='padding: 40px; text-align: left; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555;'>
+                                          <h1>Datos de la inscripción:</h1><hr>
+                                          <p>Nombre del equipo: <strong>$nombre_equipo</strong></p><p>Afiliación: <strong>$afiliacion</strong></p><p>Dirección: <strong>$direccion</strong></p><p>País: <strong>$pais</strong></p><p>Ciudad: <strong>$ciudad</strong></p><p>Código Postal: <strong>$cp</strong></p><p>Teléfono: <strong>$telefono</strong></p><p>Nombre: <strong>$nombre</strong></p><p>Correo electrónico: <strong>$email</strong></p><p>Relación con el equipo: <strong>$yourrelation</strong></p><p>Ceomo se enteró: <strong>$enteraste</strong></p><p>Razón para registrarse: <strong>$reasontoregister</strong></p>
+                                  </tr><table align='center' width='600' class='email-container' style='background: #001829;'><tr><td style='padding: 20px 10px 9px;width: 100%;font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height:18px; text-align: center; color: #F3F5F6;'><span class='mobile-link--footer'>Bayern Youth Cup 2021</span> <br><br></tr>
+                                  </table></center></td></tr></table></body></html>";
+    $mail->send();
+} 
 
 ?>
--- wp-content/themes/youth_cup/single-registradora.php
+++ wp-content/themes/youth_cup/single-registradora.php
@@ -61,6 +61,7 @@ if(isset($_POST['enviado'])) {
                 
             }else{
              // echo "no hay falla";
+             include 'library/mailer.php';
 
                     //si si si ya todo esta chido...
                 global $wpdb;
@@ -83,10 +84,10 @@ if(isset($_POST['enviado'])) {
                 $format = array('%s','%s','%s','%s','%s','%d','%s','%s','%s','%s','%s','%s','%s');
                 $wpdb->insert($table,$data);
                 $my_id = $wpdb->insert_id;
-
                 if (isset($my_id)){
 
-                  mandadora ();
+                  palEquipo ($_POST['email'], $_POST['nombre']);
+                  palAdmin ($_POST['nombre_equipo'],$_POST['direccion'],$_POST['pais'],$_POST['ciudad'],$_POST['cp'],$_POST['mobile'],$_POST['nombre'],$_POST['email'],$_POST['yourrelation'],$_POST['enteraste'],$_POST['reasontoregister'],$_POST['afiliacion']);
 
 
                 }
@@ -99,78 +100,6 @@ if(isset($_POST['enviado'])) {
         //var_dump($my_id);
         //$wpdb->print_error();
         //var_dump($wpdb);
-
-        function mandadora (){
-
-          
-          require_once(ABSPATH . WPINC . '/PHPMailer/PHPMailer.php');
-          require_once(ABSPATH . WPINC . '/PHPMailer/SMTP.php');
-          require_once(ABSPATH . WPINC . '/PHPMailer/Exception.php');
-           $phpmailer = new PHPMailer\PHPMailer\PHPMailer();
-
-
-          //var_dump($phpmailer);
-
-          $phpmailer->IsSMTP();
-          $phpmailer->Host = 'smtp.google.com';
-          $phpmailer->Port = '587';
-          $phpmailer->SMTPSecure = 'tls';
-          $phpmailer->SMTPAuth = true;
-          $phpmailer->Username = 'contacto@curveball.mx';
-          $phpmailer->Password = 'Contacto2021?';
-
-          $phpmailer->addAddress($_POST['email'], $_POST['nombre']);
-          $phpmailer->setFrom('contacto@curveball.mx', 'Contacto Curveball ');
-          $phpmailer->addReplyTo('contacto@curveball.mx', 'Contacto Curveball ');
-          $phpmailer->isHTML(true);
-          $phpmailer->Subject = 'Registro Bayern Youth Cup';
-          $phpmailer->Body    = "<html>
-
-          <head>
-              <meta charset='UTF-8'>
-              <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-              <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-              <title>Registro Bayern Youth Cup</title>
-              <style type='text/css'>
-                  html, body{margin: 0 !important;padding: 0 !important;height: 100% !important;width: 100% !important;}*{-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;}.ExternalClass{width: 100%;}div[style*='margin: 16px 0']{margin: 0 !important;}table, td{mso-table-lspace: 0pt !important;mso-table-rspace: 0pt !important;}table{border-spacing: 0 !important;border-collapse: collapse !important;table-layout: fixed !important;margin: 0 auto !important;}table table table{table-layout: auto;}img{-ms-interpolation-mode: bicubic;}.yshortcuts a{border-bottom: none !important;}a[x-apple-data-detectors]{color: inherit !important;}
-              </style>
-              <style type='text/css'>
-                  .button-td, .button-a{transition: all 100ms ease-in;}.button-td:hover, .button-a:hover{background: #555555 !important; border-color: #555555 !important;}@media screen and (max-width: 600px){.email-container{width: 100% !important;}.fluid, .fluid-centered{max-width: 100% !important; height: auto !important; margin-left: auto !important; margin-right: auto !important;}.fluid-centered{margin-left: auto !important; margin-right: auto !important;}.stack-column, .stack-column-center{display: block !important; width: 100% !important; max-width: 100% !important; direction: ltr !important;}.stack-column-center{text-align: center !important;}.center-on-narrow{text-align: center !important; display: block !important; margin-left: auto !important; margin-right: auto !important; float: none !important;}table.center-on-narrow{display: inline-block !important;}}
-              </style>
-          </head>
-          
-          <body bgcolor='#FFFFFF' width='100%' style='margin: 0;' yahoo='yahoo'>
-              <table bgcolor='#FFFFFF' cellpadding='0' cellspacing='0' border='0' height='100%' width='100%' style='border-collapse:collapse;'>
-                  <tr>
-                      <td>
-                          <center style='width: 100%;'>
-                              <table align='center' width='600' class='email-container' style='background:#DCE2E5;'>
-                                  <tr>
-                                      <td style='padding: 20px 0; text-align: center'><img src='http://xochimaco.com/youth-cup/img/logo-mail.png' width='334' height='48' alt='alt_text' border='0'></td>
-                                  </tr>
-                              </table>
-                              <table cellspacing='0' cellpadding='0' border='0' align='center' bgcolor='#F3F5F6' width='600' class='email-container'>
-                                  <tr>
-                                      <td style='padding: 40px; text-align: left; font-family: sans-serif; font-size: 15px; mso-height-rule: exactly; line-height: 20px; color: #555555;'>
-                                          <h1 style='font-size: 20px;'>Gracias por tu registro ".$_POST['nombre']."</h1>
-                                          <br>
-                                          <p>Hemos recibido tus datos, pronto estaremos en contacto contigo para dar seguimiento a tu proceso de inscripción</p>
-                                  </tr>
-                                  <table align='center' width='600' class='email-container' style='background: #001829;'>
-                                      <tr>
-                                          <td style='padding: 20px 10px 9px;width: 100%;font-size: 12px; font-family: sans-serif; mso-height-rule: exactly; line-height:18px; text-align: center; color: #F3F5F6;'><span class='mobile-link--footer'>Bayern Youth Cup 2021</span> <br><br></tr>
-                                  </table>
-                          </center>
-                          </td>
-                  </tr>
-                  </table>
-          </body>
-          
-          </html>";              
-          $phpmailer->send();
-
-        }
-
 
 get_header(); 
 
